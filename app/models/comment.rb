a50058729da17cc399e57d0d954f55cf40d16cbf
tomas-barros1 <tomasabbarros3@gmail.com>
2025-09-30 20:06:11 -0300

    validations and fixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user, optional: true
   has_rich_text :content
+  validates :content, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: [ :new, :create ]
   end
-  get "up" => "rails/health#show", as: :rails_health_check
   root "posts#index"
 end
